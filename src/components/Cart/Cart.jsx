ac89c89dbe17e7ee7bcd7b7025544397b712e29c
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-05-11 22:23:21 +0530

    Implement increment and decrement buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,54 +5,45 @@ import { useState } from "react";
 import Items from "../Items/Items";
 import style from "./Cart.module.css"
 
-export default function Cart({products, cart,setCart}){
 
-    const handleIncrement = (e)=>{
-
-        console.log(e.target.id);
-        setCart(c=>{
-            
-
-            const idx  = c.findIndex((item)=>item.id == e.target.id );
-            c[idx].quantity = c[idx].quantity + 1;
-            console.log(c);
-            return c;
-        
-        })
-    }
-    const handleDecrement = (e)=>{
-
-        console.log(e.target.id);
-        setCart(c=>{
-            
-
-            const idx  = c.findIndex((item)=>item.id == e.target.id );
-            c[idx].quantity = c[idx].quantity - 1;
-            console.log(c);
-            return c;
-        
-        })
-    }
-
-    return(
-        <div className={style.cart}>
-            {cart.map(item=>{
-                console.log("rerendered");
-            return(
-                
-                
-                <div key = {item.id} className={style['cart-item']}>
+function CartItem({item, handleDecrement,handleIncrement}){
+    if(item.quantity>0)
+    {
+        return (  <div className={style['cart-item']}>
                     
                 <img src = {item.src} alt = {item.name}/>
                 <p> {item.name[0].toUpperCase()+item.name.slice(1)} Lorem ipsum dolor sit </p>
                 <p> {item.price}</p>
                 <button className={style.decrement} id = {item.id} onClick={handleDecrement}> - </button>
-                <p>Quantity: {item.quantity}</p>
+                <p>{item.quantity}</p>
                 <button className={style.increment} id = {item.id} onClick = {handleIncrement}> + </button>
-                </div>
-            ); 
+            </div> 
+        )
+    }
+    return null;
+}
 
-            } )}
+export default function Cart({cart,setCart}){
+
+    const handleIncrement = (e)=>{
+
+
+        let idx = cart.findIndex(x => x.id === e.target.id);
+        let c = cart.slice();
+        c[idx].quantity++;
+        setCart(c);
+    }
+    const handleDecrement = (e)=>{
+
+        let idx = cart.findIndex(x => x.id === e.target.id);
+        let c = cart.slice();
+        c[idx].quantity--;
+        setCart(c);
+    }
+
+    return(
+        <div className={style.cart}>
+            {cart.map(item=>(<CartItem key={item.id} item = {item} handleIncrement={handleIncrement} handleDecrement={handleDecrement}/>))}
         </div>
     )
 }
